a36c4f6e1db27839f52324d04d61a36e2f83ab69
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2024-01-02 21:45:31 -0300

    v1

---
+++ Clipchamp_for_linux/src/main/java/com/arthur/clipchamp_for_linux/PrimaryController.java
@@ -0,0 +1,12 @@
+package com.arthur.clipchamp_for_linux;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ Clipchamp_for_linux/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.arthur.clipchamp_for_linux {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.arthur.clipchamp_for_linux to javafx.fxml;
+    exports com.arthur.clipchamp_for_linux;
+}
